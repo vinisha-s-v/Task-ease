5de43208fb59fff445651cbaa3f40c6f95338973
Your Name <vinishasv97@gmail.com>
2025-02-23 12:07:47 +0530

    bulit path add in vite.config.js

--- task-management/Fronted/task_ease/vite.config.js
+++ task-management/Fronted/task_ease/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   test: {
     globals: true, // Enables global test functions (e.g., describe, it)
     environment: 'jsdom', // Use jsdom to simulate the browser
